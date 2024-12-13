11b9515c50973a823877f0bf29e8e3f08bcc4e43
cauafyz <cauafyz@gmail.com>
2024-12-12 22:06:37 -0300

    Add files via upload

---
+++ Upload Foto (JS, HTML)/script.js
@@ -0,0 +1,19 @@
+document.getElementByIf("fotografia").onchange = function(e) {
+    if(e.target.files != null && e.target.files.length != 0){
+
+    var arquivo = e.target.files[0];
+    
+    var fd = new FormData();
+
+    fd.append("fotografia",arquivo);
+
+    var xmlhttp = new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState===4&xmlhttp.status===200)
+            alert(xmlhttp.responseText);
+
+    };
+    xmlhttp.open("POST", "recebe.php", true);
+    xmlhttp.send(fd);
+}}
